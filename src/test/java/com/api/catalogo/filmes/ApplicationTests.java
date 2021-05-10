760c761443269d320989d398248bee6562cfaf39
Leonardo Dantas <leonardo1317@yahoo.com.br>
2021-05-10 07:05:00 -0300

    Inicio do projeto

---
+++ src/test/java/com/api/catalogo/filmes/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.catalogo.filmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
