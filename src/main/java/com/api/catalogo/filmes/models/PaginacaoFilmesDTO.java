c6ad620090b1c8cd579d4ff61428f12a9fce393b
Leonardo Dantas <leonardo1317@yahoo.com.br>
2021-05-12 07:39:00 -0300

    Criação do model para recuperação dos filmes

---
+++ src/main/java/com/api/catalogo/filmes/models/PaginacaoFilmesDTO.java
@@ -0,0 +1,22 @@
+package com.api.catalogo.filmes.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class PaginacaoFilmesDTO {
+
+    @JsonProperty(value = "page")
+    private int pagina;
+
+    @JsonProperty(value = "results")
+    private List<FilmesDTO> resultados;
+
+    @JsonProperty(value = "total_pages")
+    private int totalDePaginas;
+
+    @JsonProperty(value = "total_results")
+    private int totalDeResultados;
+}
